ef91f45a1674e9105d82ae02694c6867ee38dd2c
AngelJV128a <angelj.vazquez1@gmail.com>
2025-05-06 16:59:46 -0600

    templates agregada

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS  
 from pyswip import Prolog
 import os
 
 app = Flask(__name__)
 CORS(app) 
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
 
 @app.route('/recomendar', methods=['POST'])
 def recomendar():
